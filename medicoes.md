0ec4cd4e03f7dbb558d43970c2943312b494add9
Joao <joaopedro.oliveirajava@gmail.com>
2024-06-26 00:35:04 -0300

    First commit

---
+++ medicoes.md
@@ -0,0 +1,34 @@
+Medido: 124.2V 3.64A P 452 fp 1 - lampadas
+Vrms: 124.373 (V); Irms: 3.609 (I); Prms: 444.932 0.99
+Vrms: 124.386 (V); Irms: 3.614 (I); Prms: 445.586 0.99
+Vrms: 124.326 (V); Irms: 3.599 (I); Prms: 443.566 0.99
+Vrms: 124.507 (V); Irms: 3.631 (I); Prms: 448.607 0.99
+Vrms: 124.385 (V); Irms: 3.610 (I); Prms: 445.488 0.99
+Vrms: 124.105 (V); Irms: 3.627 (I); Prms: 445.264 0.99
+Vrms: 124.425 (V); Irms: 3.608 (I); Prms: 445.276 0.99
+
+Carga RC1: 124V 3.86A 451W 0.94fp
+Vrms: 124.418 (V); Irms: 3.799 (I); Prms: 426.800 0.90
+Vrms: 124.419 (V); Irms: 3.828 (I); Prms: 431.143 0.91
+Vrms: 124.308 (V); Irms: 3.834 (I); Prms: 433.176 0.91
+Vrms: 124.369 (V); Irms: 3.852 (I); Prms: 434.544 0.91
+Vrms: 124.244 (V); Irms: 3.826 (I); Prms: 430.064 0.90
+Vrms: 124.222 (V); Irms: 3.828 (I); Prms: 430.891 0.91
+Vrms: 124.164 (V); Irms: 3.825 (I); Prms: 430.922 0.91
+
+Carga RC2: 124.2 4.43A 454W 0.82fp
+Vrms: 124.407 (V); Irms: 4.403 (I); Prms: 423.401 0.77
+Vrms: 124.350 (V); Irms: 4.429 (I); Prms: 416.810 0.76
+Vrms: 124.280 (V); Irms: 4.389 (I); Prms: 423.772 0.78
+Vrms: 124.236 (V); Irms: 4.354 (I); Prms: 416.554 0.77
+Vrms: 124.361 (V); Irms: 4.374 (I); Prms: 413.530 0.76
+Vrms: 124.474 (V); Irms: 4.365 (I); Prms: 419.422 0.77
+
+Carga RC3: 124.2V 4.8A 455W 0.76fp
+Vrms: 124.568 (V); Irms: 4.744 (I); Prms: 414.420 0.70
+Vrms: 124.555 (V); Irms: 4.771 (I); Prms: 414.617 0.70
+Vrms: 124.561 (V); Irms: 4.799 (I); Prms: 416.718 0.70
+Vrms: 124.507 (V); Irms: 4.797 (I); Prms: 417.077 0.70
+Vrms: 124.418 (V); Irms: 4.792 (I); Prms: 417.055 0.70
+Vrms: 124.440 (V); Irms: 4.754 (I); Prms: 415.651 0.70
+Vrms: 124.456 (V); Irms: 4.755 (I); Prms: 414.718 0.70
---
+++ src/main.cpp
@@ -0,0 +1,186 @@
+#include <Arduino.h>
+
+#define CLOCKRATE 80000000 /* Hz */
+#define TIMERDIVIDER 4
+
+#define OVER_SAMPLE_RATIO (16)
+#define CYCLES (20)
+#define NSAMPLES (OVER_SAMPLE_RATIO * CYCLES) // 321
+
+#define ADC_BITS 12
+#define ADC_COUNTS (1 << ADC_BITS)
+
+#define VOLTAGE_ADC_PIN (34)
+#define CURRENT_ADC_PIN (35)
+
+volatile int sampleCount = NSAMPLES;
+volatile int voltageSamples[NSAMPLES];
+volatile int currentSamples[NSAMPLES];
+
+hw_timer_t *My_timer = NULL;
+
+struct ElectricalMeasurements {
+  double vrms;
+  double irms;
+  double realPower;
+  double apparentPower;
+  double powerFactor;
+};
+
+struct ElectricalMeasurements measurements;
+
+void setupMeasurement();
+struct ElectricalMeasurements makeMeasurement();
+
+double amountMeasurements;
+double sumVrms;
+double sumIrms;
+double sumRealPower;
+double sumApparentPower;
+double sumPowerFactor;
+
+int countRmsMeasurements = 0;
+double sumVoltageToSend = 0.0;
+double sumCurrentToSend = 0.0;
+double sumRealPowerToSend = 0.0;
+
+void setup() {
+  Serial.begin(115200);
+  setupMeasurement();
+}
+
+void loop() {
+  measurements = makeMeasurement();
+  if (countRmsMeasurements < 3) {
+    sumVoltageToSend += measurements.vrms;
+    sumCurrentToSend += measurements.irms;
+    sumRealPowerToSend += measurements.realPower;
+    sumApparentPower += measurements.vrms * measurements.irms;
+
+    countRmsMeasurements++;
+    
+    return;
+  }
+  Serial.print("Vrms: ");
+  Serial.print(sumVoltageToSend / 3.0, 3);
+  Serial.print(" (V); Irms: ");
+  Serial.print(sumCurrentToSend / 3.0, 3);
+  Serial.print(" (I); Prms: ");
+  Serial.print(sumRealPowerToSend / 3.0, 3);
+  Serial.print(" ");
+  float powerFactorConverted = (sumRealPowerToSend / 3.0) / (sumApparentPower / 3.0);
+  Serial.println(powerFactorConverted);
+
+  sumVoltageToSend = 0;
+  sumCurrentToSend = 0;
+  sumRealPowerToSend = 0;
+  sumApparentPower = 0;
+
+  countRmsMeasurements = 0;
+}
+
+void IRAM_ATTR onTimer() {
+  if ((sampleCount >= 0) && (sampleCount < (NSAMPLES))) {
+    voltageSamples[sampleCount] = analogRead(VOLTAGE_ADC_PIN);
+    currentSamples[sampleCount] = analogRead(CURRENT_ADC_PIN);
+
+    sampleCount++;
+  }
+}
+
+void setupMeasurement() {
+  My_timer = timerBegin(
+    1,
+    TIMERDIVIDER,
+    true
+  );
+
+  timerAttachInterrupt(My_timer, &onTimer, true);
+
+  float measureRatePerInterval = 1.0 / ( 60.0 * OVER_SAMPLE_RATIO);
+
+  int amountTimeBetweenInterruption = (int)( measureRatePerInterval * CLOCKRATE / TIMERDIVIDER + 0.5);
+
+  timerAlarmWrite(My_timer, amountTimeBetweenInterruption, true);
+
+  timerAlarmEnable(My_timer);
+}
+
+void readAnalogSamples() {
+  int waitDelay = 17 * CYCLES;
+  sampleCount = 0;
+
+  delay(waitDelay); // 340 ms
+  if (sampleCount != NSAMPLES) {
+    Serial.print("ADC processing is not working.");
+  }
+
+  timerWrite(My_timer, 0);
+}
+
+struct ElectricalMeasurements measureRms(int* voltageSamples, int* currentSamples, int nsamples) {
+  float numberOfTurnsCT = 2.0;
+  struct ElectricalMeasurements eletricMeasurements;
+  // int32_t sumVoltageSamples = 0;
+  // int32_t sumCurrentSamples = 0;
+
+  // for (int i = 0; i < nsamples; i++) {
+  //   Serial.print(voltageSamples[i]);
+  //   Serial.print(" ");
+  //   Serial.println(currentSamples[i]);
+  //   sumVoltageSamples += voltageSamples[i];
+  //   sumCurrentSamples += currentSamples[i];
+  // }
+  int offsetVoltage = 1851;
+  int offsetCurrent = 1848;
+  // int voltageMean = (int)(sumVoltageSamples / (int32_t)(nsamples));
+  // int currentMean = (int)(sumCurrentSamples / (int32_t)(nsamples));
+
+  float sumVoltage = 0;
+  float sumCurrent = 0;
+  float sumInstantaneousPower = 0;
+  for (int i = 0; i < nsamples; i++) {
+    int y_voltageNoOffset = voltageSamples[i] - offsetVoltage;
+    int y_currentNoOffset = currentSamples[i] - offsetCurrent;
+
+    float y_voltage = ((float)y_voltageNoOffset) * (282.5) * (3.3/4096.0);
+    float y_current = ((float)y_currentNoOffset) * (30.5) * (3.3/4096.0);
+
+    // y_current = y_current / numberOfTurnsCT;
+
+    float y_instantaneousPower = y_voltage * y_current;
+  
+    sumVoltage += y_voltage * y_voltage;
+    sumCurrent += y_current * y_current;
+    sumInstantaneousPower += y_instantaneousPower;
+  }
+
+  float ym_voltage = sumVoltage / (float) nsamples;
+  float ym_current = sumCurrent / (float) nsamples;
+  float ym_realPower = sumInstantaneousPower / (float) nsamples;
+
+  float vrmsSquared = sqrt(ym_voltage);
+  float irmsSquared = sqrt(ym_current);
+
+  float Vrms = vrmsSquared;
+  float Irms = irmsSquared;
+
+  float realPower = ym_realPower;
+
+  eletricMeasurements.vrms = Vrms;
+  eletricMeasurements.irms = Irms;
+  eletricMeasurements.realPower = realPower;
+
+  return eletricMeasurements;
+}
+
+struct ElectricalMeasurements makeMeasurement() {
+  struct ElectricalMeasurements eletricMeasurements;
+
+  readAnalogSamples();
+  if (sampleCount == NSAMPLES) {
+    eletricMeasurements = measureRms((int*) voltageSamples, (int*) currentSamples, NSAMPLES);
+  }
+
+  return eletricMeasurements;
+}
